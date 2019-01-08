439e59db76e92c4a5c31ff0589a093e908dce6cc
martin1123 <martinmaccio89@gmail.com>
2019-01-07 22:28:12 -0300

    Inicio del proyecto

---
+++ figuras/src/main/java/ar/com/geopagos/figuras/FigurasApplication.java
@@ -0,0 +1,14 @@
+package ar.com.geopagos.figuras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FigurasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FigurasApplication.class, args);
+	}
+
+}
+
