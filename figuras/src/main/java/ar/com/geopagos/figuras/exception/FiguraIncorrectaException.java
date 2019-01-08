c4ed9ee146f224211b34372d16c0718c9827b304
martin1123 <martinmaccio89@gmail.com>
2019-01-08 06:42:11 -0300

    Se agregan excepciones

---
+++ figuras/src/main/java/ar/com/geopagos/figuras/exception/FiguraIncorrectaException.java
@@ -0,0 +1,13 @@
+package ar.com.geopagos.figuras.exception;
+
+public class FiguraIncorrectaException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public FiguraIncorrectaException(String msg) {
+		super(msg);
+	}
+}
